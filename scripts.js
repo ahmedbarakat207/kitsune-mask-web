3b84ab51cd14da255c0e9f2a901a1bef0acd5649
Ahmed Barakat <147771950+ahmedbarakat207@users.noreply.github.com>
2026-01-16 15:52:12 +0200

    initial add

---
+++ scripts.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        kitsune: {
+                            50: '#fff7ed',
+                            100: '#ffedd5',
+                            500: '#c71616', 
+                            600: '#a31f1f', 
+                            900: '#7c2d12',
+                        },
+                        darkbg: '#0f0f11',
+                        cardbg: '#18181b',
+                    },
+                    fontFamily: {
+                        sans: ['Inter', 'sans-serif'],
+                    }
+                }
+            }
+        }
